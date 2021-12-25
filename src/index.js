8a519f014db6ca1667696c881c540c5eefab6f72
Seun Soetan <57603735+adewemimo@users.noreply.github.com>
2021-12-25 00:44:25 -0500

    Include useEffects

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ import Fixed from "./components/Fixed";
 
 function Application() {
   const [likes, setLikes] = useState(0);
+  useEffect(() => {
+    console.log(`Like Count: ${likes}`);
+    document.body.className = `bg--${likes % 2}`;
+  }, [likes]);
+
+  useEffect(() => {
+    if (likes > 0) {
+      const timeOut = setTimeout((prev) => setLikes(prev - 1), 1000);
+      return () => clearTimeout(timeOut);
+    }
+  }, [likes]);
 
   return (
     <div onClick={() => setLikes(likes + 1)}>
